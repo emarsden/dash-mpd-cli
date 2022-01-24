a4b26643173fef4102ba3bd1fe373cd5cd8cf874
Eric Marsden <eric.marsden@foncsi.org>
2022-01-24 16:03:08 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,190 @@
+# dash-mpd-cli
+
+A commandline application for downloading media content from a DASH MPD file, as used for on-demand
+replay of TV content and video streaming services like YouTube.
+
+[![Crates.io](https://img.shields.io/crates/v/dash-mpd-cli)](https://crates.io/crates/dash-mpd-cli)
+[![Released API docs](https://docs.rs/dash-mpd-cli/badge.svg)](https://docs.rs/dash-mpd-cli/)
+[![CI](https://github.com/emarsden/dash-mpd-cli/workflows/build/badge.svg)](https://github.com/emarsden/dash-mpd-cli/workflows/build/badge.svg)
+[![Dependency status](https://deps.rs/repo/github/emarsden/dash-mpd-cli/status.svg)](https://deps.rs/repo/github/emarsden/dash-mpd-cli)
+[![LICENSE](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE-MIT)
+
+![Terminal capture](https://risk-engineering.org/emarsden/dash-mpd-cli/terminal-capture.svg)
+
+
+[DASH](https://en.wikipedia.org/wiki/Dynamic_Adaptive_Streaming_over_HTTP) (dynamic adaptive
+streaming over HTTP), also called MPEG-DASH, is a technology used for media streaming over the web,
+commonly used for video on demand (VOD) services. The Media Presentation Description (MPD) is a
+description of the resources (manifest or “playlist”) forming a streaming service, that a DASH
+client uses to determine which assets to request in order to perform adaptive streaming of the
+content. DASH MPD manifests can be used both with content encoded as MPEG and as WebM. There is a
+good explanation of adaptive bitrate video streaming at
+[howvideo.works](https://howvideo.works/#dash).
+
+This commandline application allows you to download content (audio or video) described by an MPD
+manifest. This involves selecting the alternative with the most appropriate encoding (in terms of
+bitrate, codec, etc.), fetching segments of the content using HTTP or HTTPS requests and muxing
+audio and video segments together. It builds on the [dash-mpd](https://crates.io/crates/dash-mpd)
+crate.
+
+
+## Installation
+
+```shell
+cargo install dash-mpd-cli
+```
+
+
+## Usage
+
+```
+dash-mpd-cli 0.1.0
+Download content from a DASH streaming media manifest
+
+USAGE:
+    dash-mpd-cli [OPTIONS] <MPD-URL>
+
+ARGS:
+    <MPD-URL>    URL of the DASH manifest to retrieve
+
+OPTIONS:
+        --add-header <add-header>
+            Add a custom HTTP header and its value, separated by a colon ':'. You can use this
+            option multiple times.
+
+    -h, --help
+            Print help information
+
+        --no-progress
+            Disable the progress bar
+
+        --no-xattr
+            Don't record metainformation as extended attributes in the output file
+
+    -o, --output <output>
+            Save media content to this file
+
+        --proxy <proxy>
+            
+
+    -q, --quiet
+            
+
+        --quality <quality>
+            Prefer best quality (and highest bandwidth) representation, or lowest quality [possible
+            values: best, worst]
+
+        --source-address <source-address>
+            Source IP address to use for network requests, either IPv4 or IPv6. Network requests
+            will be made using the version of this IP address (eg. using an IPv6 source-address will
+            select IPv6 network traffic).
+
+        --timeout <timeout>
+            Timeout for network requests (from the start to the end of the request), in seconds
+
+        --user-agent <user-agent>
+            
+
+    -v, --verbose
+            Level of verbosity (can be used several times)
+
+        --version
+```
+
+
+If your filesystem supports extended attributes, the application will save the following
+metainformation:
+
+- `user.xdg.origin.url`: the URL of the MPD manifest
+- `user.dublincore.title`: the title, if specified in the manifest metainformation
+- `user.dublincore.source`: the source, if specified in the manifest metainformation
+- `user.dublincore.rights`: copyright information, if specified in the manifest metainformation
+
+You can examine these attributes using `xattr -l` (you may need to install your distribution's
+`xattr` package).
+
+
+## Platforms
+
+This crate is tested on the following platforms:
+
+- Linux
+- MacOS
+- Microsoft Windows 10
+
+The underlying library `dash-mpd-rs` has two methods for muxing audio and video streams together. If
+the library feature `libav` is enabled (which is not the default configuration), muxing support is
+provided by ffmpeg’s libav library, via the `ac_ffmpeg` crate. Otherwise, muxing is implemented by
+calling `ffmpeg` (and if that fails, `vlc`) as a subprocess. The ffmpeg commandline application
+implements a number of checks and workarounds to fix invalid input streams that tend to exist in the
+wild. Some of these workarounds, but not all, are implemented here when using libav as a library, so
+download support tends to be more robust with the default configuration (using ffmpeg or vlc as a
+subprocess). The `libav` feature currently only works on Linux.
+
+
+
+## DASH features supported
+
+- VOD (static) stream manifests
+- Multi-period content
+- XLink elements (only with actuate=onLoad semantics), including resolve-to-zero
+- All forms of segment index info: SegmentBase@indexRange, SegmentTimeline,
+  SegmentTemplate@duration, SegmentTemplate@index, SegmentList
+- Media containers of types supported by ffmpeg or VLC (this includes ISO-BMFF / CMAF / MP4, WebM, MPEG-2 TS)
+
+
+## Limitations / unsupported features
+
+- Dynamic MPD manifests, that are used for live streaming/OTT TV
+- Encrypted content using DRM such as Encrypted Media Extensions (EME) and Media Source Extension (MSE)
+- Subtitles (eg. WebVTT and TTML streams)
+- XLink with actuate=onRequest
+
+
+
+## License
+
+This project is licensed under the MIT license. For more information, see the `LICENSE-MIT` file.
+
+
+
+## Similar tools
+
+Similar commandline tools that are able to download content from a DASH manifest
+
+- youtube-dl and forks like yt-dlp are able to download content from a DASH manifest
+
+- streamlink -o /tmp/output.mp4 <MPD-URL> worst
+
+- ffmpeg -i <MPD-URL> -vcodec copy /tmp/output.mp4
+
+- vlc <MPD-URL>
+
+- gst-launch-1.0 playbin uri=<MPD-URL>
+
+This application is able to download content from certain streams that do not work with other
+applications (for example xHE-AAC streams which are unsupported by ffmpeg, streamlink, VLC,
+gstreamer).
+
+
+## Building
+
+```
+$ git clone https://github.com/emarsden/dash-mpd-cli
+$ cd dash-mpd-cli
+$ cargo build --release
+$ target/release/dash-mpd-cli --help
+```
+
+The application can also be built statically with the musl-libc target on Linux. First install the
+MUSL library on your system. Add linux-musl as a target to your Rust toolchain, then rebuild for the
+relevant target:
+
+```
+$ sudo apt install musl-dev
+$ rustup target add x86_64-unknown-linux-musl
+$ cargo build --release --target x86_64-unknown-linux-musl
+```
+
+Static musl-libc builds don't work with OpenSSL, which is why we disable default features on the
+reqwest crate and build it with rustls-tls support. 
---
+++ src/main.rs
@@ -0,0 +1,205 @@
+//! dash-mpd-cli
+//!
+//! A commandline application for downloading media content from a DASH MPD file, as used for on-demand
+//! replay of TV content and video streaming services like YouTube.
+//!
+//! [DASH](https://en.wikipedia.org/wiki/Dynamic_Adaptive_Streaming_over_HTTP) (dynamic adaptive
+//! streaming over HTTP), also called MPEG-DASH, is a technology used for media streaming over the web,
+//! commonly used for video on demand (VOD) services. The Media Presentation Description (MPD) is a
+//! description of the resources (manifest or “playlist”) forming a streaming service, that a DASH
+//! client uses to determine which assets to request in order to perform adaptive streaming of the
+//! content. DASH MPD manifests can be used both with content encoded as MPEG and as WebM.
+//!
+//! This commandline application allows you to download content (audio or video) described by an MPD
+//! manifest. This involves selecting the alternative with the most appropriate encoding (in terms of
+//! bitrate, codec, etc.), fetching segments of the content using HTTP or HTTPS requests and muxing
+//! audio and video segments together. It builds on the [dash-mpd](https://crates.io/crates/dash-mpd)
+//! crate.
+//
+//
+// Example usage: dash-mpd-cli --timeout 5 --output=/tmp/foo.mp4 https://v.redd.it/zv89llsvexdz/DASHPlaylist.mpd
+
+use std::env;
+use std::net::IpAddr;
+use std::str::FromStr;
+use std::time::Duration;
+use std::sync::Arc;
+use std::collections::HashMap;
+use env_logger::Env;
+use reqwest::header;
+use clap::Arg;
+use indicatif::{ProgressBar, ProgressStyle};
+use anyhow::Result;
+use dash_mpd::fetch::DashDownloader;
+use dash_mpd::fetch::ProgressObserver;
+
+
+struct DownloadProgressBar {
+    bar: ProgressBar,
+}
+
+impl DownloadProgressBar {
+    pub fn new() -> Self {
+        let b = ProgressBar::new(100)
+            .with_style(ProgressStyle::default_bar()
+                        .template("[{elapsed}] {bar:50.cyan/blue} {wide_msg}")
+                        .progress_chars("#>-"));
+        Self { bar: b }
+    }
+}
+
+impl ProgressObserver for DownloadProgressBar {
+    fn update(&self, percent: u32, message: &str) {
+        if percent <= 100 {
+            self.bar.set_position(percent.into());
+            self.bar.set_message(message.to_string());
+        }
+        if percent == 100 {
+            self.bar.finish_with_message("Done");
+        }
+    }
+}
+
+
+fn main () -> Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info,reqwest=warn")).init();
+    let matches = clap::App::new("dash-mpd-cli")
+        .about("Download content from a DASH streaming media manifest")
+        .version("0.1.0")
+        .arg(Arg::new("user-agent")
+             .long("user-agent")
+             .takes_value(true))
+        .arg(Arg::new("proxy")
+             .long("proxy")
+             .takes_value(true))
+        .arg(Arg::new("timeout")
+             .long("timeout")
+             .takes_value(true)
+             .help("Timeout for network requests (from the start to the end of the request), in seconds"))
+        .arg(Arg::new("source-address")
+             .long("source-address")
+             .takes_value(true)
+	     .help("Source IP address to use for network requests, either IPv4 or IPv6. Network requests will be made using the version of this IP address (eg. using an IPv6 source-address will select IPv6 network traffic)."))
+        .arg(Arg::new("quality")
+             .long("quality")
+             .takes_value(true)
+             .possible_value("best")
+             .possible_value("worst")
+             .help("Prefer best quality (and highest bandwidth) representation, or lowest quality"))
+        .arg(Arg::new("add-header")
+             .long("add-header")
+             .takes_value(true)
+             .multiple_occurrences(true)
+             .help("Add a custom HTTP header and its value, separated by a colon ':'. You can use this option multiple times."))
+        .arg(Arg::new("quiet")
+             .short('q')
+             .long("quiet")
+             .conflicts_with("verbose")
+             .takes_value(false))
+        .arg(Arg::new("verbose")
+             .short('v')
+             .long("verbose")
+             .takes_value(false)
+             .multiple_occurrences(true)
+             .help("Level of verbosity (can be used several times)"))
+        .arg(Arg::new("no-progress")
+             .long("no-progress")
+             .takes_value(false)
+             .help("Disable the progress bar"))
+        .arg(Arg::new("no-xattr")
+             .long("no-xattr")
+             .takes_value(false)
+             .help("Don't record metainformation as extended attributes in the output file"))
+        .arg(Arg::new("version")
+             .long("version")
+             .takes_value(false))
+        .arg(Arg::new("output")
+             .long("output")
+             .short('o')
+             .takes_value(true)
+             .help("Save media content to this file"))
+        .arg(Arg::new("url")
+             .takes_value(true)
+             .value_name("MPD-URL")
+             .required(true)
+             .index(1)
+             .help("URL of the DASH manifest to retrieve"))
+        .get_matches();
+    // TODO: add --abort-on-error
+    // TODO: add --fragment-retries arg
+    // TODO: add --mtime arg (Last-modified header)
+    // TODO: limit download rate once reqwest crate can do so
+    let verbosity = matches.occurrences_of("verbose") as u8;
+    let ua = matches.value_of("user-agent")
+        .unwrap_or(concat!("dash-mpd-cli/", env!("CARGO_PKG_VERSION")));
+    let mut cb = reqwest::blocking::Client::builder()
+        .user_agent(ua)
+	.tcp_nodelay(true)
+        .gzip(true)
+        .brotli(true);
+    if verbosity > 2 {
+       cb = cb.connection_verbose(true);
+    }
+    if let Some(p) = matches.value_of("proxy") {
+        let proxy = reqwest::Proxy::http(p)
+            .expect("Can't connect to HTTP proxy");
+        cb = cb.proxy(proxy);
+    }
+    if let Some(src) = matches.value_of("source-address") {
+       if let Ok(local_addr) = IpAddr::from_str(src) {
+          cb = cb.local_address(local_addr);
+       } else {
+          eprintln!("Ignoring invalid argument to --source-address: {}", src);
+       }
+    }
+    if let Some(seconds) = matches.value_of("timeout") {
+        if let Ok(secs) = seconds.parse::<u64>() {
+            cb = cb.timeout(Duration::new(secs, 0));
+        } else {
+            eprintln!("Ignoring invalid value for --timeout: {}", seconds);
+        }
+    } else {
+        cb = cb.timeout(Duration::new(30, 0));
+    }
+
+    if let Some(hvs) = matches.values_of("add-header") {
+        let mut headers = HashMap::new();
+        for hv in hvs.collect::<Vec<_>>() {
+            if let Some(pos) = hv.find(':') {
+                let (h, v) = hv.split_at(pos);
+                headers.insert(h.to_string(), v.to_string());
+            } else {
+                eprintln!("Ignoring badly formed header:value argument to --add-header");
+            }
+        }
+        let hmap: header::HeaderMap = (&headers).try_into()
+            .expect("valid HTTP headers");
+        cb = cb.default_headers(hmap);
+    }
+
+    let client = cb.build()
+        .expect("Couldn't create reqwest HTTP client");
+    let url = matches.value_of("url").unwrap();
+    let mut dl = DashDownloader::new(url)
+        .with_http_client(client);
+    if !matches.is_present("no-progress") && !matches.is_present("quiet") {
+        dl = dl.add_progress_observer(Arc::new(DownloadProgressBar::new()));
+    }
+    if matches.is_present("no-xattr") {
+        dl = dl.record_metainformation(false)
+    }
+    if let Some(q) = matches.value_of("quality") {
+        if q.eq("best") {
+            // DashDownloader defaults to worst quality
+            dl = dl.best_quality();
+        }
+    }
+    dl = dl.verbosity(verbosity);
+    if let Some(out) = matches.value_of("output") {
+        dl.download_to(out)?;
+    } else {
+        let out = dl.download()?;
+        println!("Downloaded DASH content to {:?}", out);
+    }
+    std::process::exit(0)
+}
