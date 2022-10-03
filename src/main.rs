2d6f8de6168eaacbdd03d78c9d62cb55af2edfde
Eric Marsden <eric.marsden@risk-engineering.org>
2022-10-03 14:42:12 +0200

    Update clap dependency to 4.x

--- src/main.rs
+++ src/main.rs
@@ -69,90 +69,95 @@ fn main () -> Result<()> {
         .version(clap::crate_version!())
         .arg(Arg::new("user-agent")
              .long("user-agent")
-             .takes_value(true))
+             .num_args(1))
         .arg(Arg::new("proxy")
              .long("proxy")
-             .takes_value(true))
+             .value_name("URL")
+             .num_args(1)
+             .help("URL of Socks or HTTP proxy (eg. https://example.net/ or socks5://example.net/)"))
         .arg(Arg::new("timeout")
              .long("timeout")
-             .takes_value(true)
+             .value_name("SECONDS")
+             .num_args(1)
              .help("Timeout for network requests (from the start to the end of the request), in seconds"))
-        .arg(Arg::new("sleep-seconds")
+        .arg(Arg::new("sleep-requests")
              .long("sleep-requests")
-             .takes_value(true)
+             .value_name("SECONDS")
+             .num_args(1)
              .help("Number of seconds to sleep between network requests (default 0)"))
         .arg(Arg::new("source-address")
              .long("source-address")
-             .takes_value(true)
+             .num_args(1)
 	     .help("Source IP address to use for network requests, either IPv4 or IPv6. Network requests will be made using the version of this IP address (eg. using an IPv6 source-address will select IPv6 network traffic)."))
         .arg(Arg::new("quality")
              .long("quality")
-             .takes_value(true)
-             .possible_value("best")
-             .possible_value("worst")
+             .num_args(1)
+             .value_parser(["best", "worst"])
              .help("Prefer best quality (and highest bandwidth) representation, or lowest quality"))
-        .arg(Arg::new("lang")
+        .arg(Arg::new("prefer-language")
              .long("prefer-language")
-             .takes_value(true)
+             .value_name("LANG")
+             .num_args(1)
              .help("Preferred language when multiple audio streams with different languages are available. Must be in RFC 5646 format (eg. fr or en-AU). If a preference is not specified and multiple audio streams are present, the first one listed in the DASH manifest will be downloaded."))
         .arg(Arg::new("video-only")
              .long("video-only")
-             .takes_value(false)
+             .num_args(0)
              .conflicts_with("audio-only")
              .help("If the media stream has separate audio and video streams, only download the video stream"))
         .arg(Arg::new("audio-only")
              .long("audio-only")
-             .takes_value(false)
+             .num_args(0)
              .conflicts_with("video-only")
              .help("If the media stream has separate audio and video streams, only download the audio stream"))
-        .arg(Arg::new("extra-header")
+        .arg(Arg::new("add-header")
              .long("add-header")
-             .takes_value(true)
-             .multiple_occurrences(true)
+             .value_name("NAME:VALUE")
+             .num_args(1)
+             .action(clap::ArgAction::Append)
              .help("Add a custom HTTP header and its value, separated by a colon ':'. You can use this option multiple times."))
         .arg(Arg::new("quiet")
              .short('q')
              .long("quiet")
-             .conflicts_with("verbose")
-             .takes_value(false))
+             .num_args(0)
+             .conflicts_with("verbose"))
         .arg(Arg::new("verbose")
              .short('v')
              .long("verbose")
-             .takes_value(false)
-             .multiple_occurrences(true)
+             .action(clap::ArgAction::Count)
              .help("Level of verbosity (can be used several times)"))
         .arg(Arg::new("no-progress")
              .long("no-progress")
-             .takes_value(false)
+             .num_args(0)
              .help("Disable the progress bar"))
         .arg(Arg::new("no-xattr")
              .long("no-xattr")
-             .takes_value(false)
+             .num_args(0)
              .help("Don't record metainformation as extended attributes in the output file"))
         .arg(Arg::new("ffmpeg-location")
              .long("ffmpeg-location")
-             .takes_value(true)
+             .value_name("PATH")
+             .num_args(1)
              .help("Path to the ffmpeg binary (necessary if not located in your PATH)"))
         .arg(Arg::new("vlc-location")
              .long("vlc-location")
-             .takes_value(true)
+             .value_name("PATH")
+             .num_args(1)
              .help("Path to the VLC binary (necessary if not located in your PATH)"))
         .arg(Arg::new("mkvmerge-location")
              .long("mkvmerge-location")
-             .takes_value(true)
+             .value_name("PATH")
+             .num_args(1)
              .help("Path to the mkvmerge binary (necessary if not located in your PATH)"))
-        .arg(Arg::new("version")
-             .long("version")
-             .takes_value(false))
         .arg(Arg::new("output-file")
              .long("output")
+             .value_name("PATH")
              .short('o')
-             .takes_value(true)
+             .num_args(1)
              .help("Save media content to this file"))
         .arg(Arg::new("url")
-             .takes_value(true)
              .value_name("MPD-URL")
              .required(true)
+             .num_args(1)
              .index(1)
              .help("URL of the DASH manifest to retrieve"))
         .get_matches();
@@ -160,9 +165,11 @@ fn main () -> Result<()> {
     // TODO: add --fragment-retries arg
     // TODO: add --mtime arg (Last-modified header)
     // TODO: limit download rate once reqwest crate can do so
-    let verbosity = matches.occurrences_of("verbose") as u8;
-    let ua = matches.value_of("user-agent")
-        .unwrap_or(concat!("dash-mpd-cli/", env!("CARGO_PKG_VERSION")));
+    let verbosity = matches.get_count("verbose") as u8;
+    let ua = match matches.get_one::<String>("user-agent") {
+        Some(ua) => ua,
+        None => concat!("dash-mpd-cli/", env!("CARGO_PKG_VERSION")),
+    };
     let mut cb = reqwest::blocking::Client::builder()
         .user_agent(ua)
 	.tcp_nodelay(true)
@@ -171,19 +178,19 @@ fn main () -> Result<()> {
     if verbosity > 2 {
        cb = cb.connection_verbose(true);
     }
-    if let Some(p) = matches.value_of("proxy") {
-        let proxy = reqwest::Proxy::http(p)
-            .expect("Can't connect to HTTP proxy");
+    if let Some(p) = matches.get_one::<String>("proxy") {
+        let proxy = reqwest::Proxy::all(p)
+            .expect("connecting to HTTP proxy");
         cb = cb.proxy(proxy);
     }
-    if let Some(src) = matches.value_of("source-address") {
+    if let Some(src) = matches.get_one::<String>("source-address") {
        if let Ok(local_addr) = IpAddr::from_str(src) {
           cb = cb.local_address(local_addr);
        } else {
           eprintln!("Ignoring invalid argument to --source-address: {}", src);
        }
     }
-    if let Some(seconds) = matches.value_of("timeout") {
+    if let Some(seconds) = matches.get_one::<String>("timeout") {
         if let Ok(secs) = seconds.parse::<u64>() {
             cb = cb.timeout(Duration::new(secs, 0));
         } else {
@@ -192,7 +199,7 @@ fn main () -> Result<()> {
     } else {
         cb = cb.timeout(Duration::new(30, 0));
     }
-    if let Some(hvs) = matches.values_of("extra-header") {
+    if let Some(hvs) = matches.get_many::<String>("add-header") {
         let mut headers = HashMap::new();
         for hv in hvs.collect::<Vec<_>>() {
             if let Some(pos) = hv.find(':') {
@@ -208,49 +215,45 @@ fn main () -> Result<()> {
     }
 
     let client = cb.build()
-        .expect("Couldn't create reqwest HTTP client");
-    let url = matches.value_of("url").unwrap();
+        .expect("creating reqwest HTTP client");
+    let url = matches.get_one::<String>("url").unwrap();
     let mut dl = DashDownloader::new(url)
         .with_http_client(client);
-    if !matches.is_present("no-progress") && !matches.is_present("quiet") {
+    if !matches.contains_id("no-progress") && !matches.contains_id("quiet") {
         dl = dl.add_progress_observer(Arc::new(DownloadProgressBar::new()));
     }
-    if let Some(seconds) = matches.value_of("sleep-seconds") {
-        if let Ok(secs) = seconds.parse::<u8>() {
-            dl = dl.sleep_between_requests(secs);
-        } else {
-            eprintln!("Ignoring invalid value for --sleep-requests: {}", seconds);
-        }
+    if let Some(seconds) = matches.get_one::<u8>("sleep-requests") {
+        dl = dl.sleep_between_requests(*seconds);
     }
-    if matches.is_present("audio-only") {
+    if matches.contains_id("audio-only") {
         dl = dl.audio_only();
     }
-    if matches.is_present("video-only") {
+    if matches.contains_id("video-only") {
         dl = dl.video_only();
     }
-    if matches.is_present("no-xattr") {
+    if matches.contains_id("no-xattr") {
         dl = dl.record_metainformation(false);
     }
-    if let Some(ffmpeg_path) = matches.value_of("ffmpeg-location") {
+    if let Some(ffmpeg_path) = matches.get_one::<String>("ffmpeg-location") {
         dl = dl.with_ffmpeg(ffmpeg_path);
     }
-    if let Some(path) = matches.value_of("vlc-location") {
+    if let Some(path) = matches.get_one::<String>("vlc-location") {
         dl = dl.with_vlc(path);
     }
-    if let Some(path) = matches.value_of("mkvmerge-location") {
+    if let Some(path) = matches.get_one::<String>("mkvmerge-location") {
         dl = dl.with_mkvmerge(path);
     }
-    if let Some(q) = matches.value_of("quality") {
+    if let Some(q) = matches.get_one::<String>("quality") {
         if q.eq("best") {
             // DashDownloader defaults to worst quality
             dl = dl.best_quality();
         }
     }
-    if let Some(lang) = matches.value_of("lang") {
+    if let Some(lang) = matches.get_one::<String>("prefer-language") {
         dl = dl.prefer_language(lang.to_string());
     }
     dl = dl.verbosity(verbosity);
-    if let Some(out) = matches.value_of("output-file") {
+    if let Some(out) = matches.get_one::<String>("output-file") {
         dl.download_to(out)?;
     } else {
         let out = dl.download()?;
