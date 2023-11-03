4e583b737e614048e3dfbec75347d1d86d140547
Eric Marsden <eric.marsden@risk-engineering.org>
2023-11-03 18:28:38 +0100

    Improve certificate generation script for tests

--- tests/create-certs.sh
+++ tests/create-certs.sh
@@ -9,42 +9,45 @@
 #
 # To dump the content of a certificate, openssl x509 -in cert.csr -text
 
-openssl genrsa -out root-CA.key 4096
-openssl genrsa -out localhost-cert.key 4096
-openssl genrsa -out client-cert.key 4096
+openssl genrsa -aes256 -out root-CA.key 4096
+openssl genrsa -aes256 -out localhost-cert.key 4096
+openssl genrsa -aes256 -out client-cert.key 4096
 
 
 # create the certificate for the root Certificate Authority
 openssl req -x509 -new -nodes \
-   -sha256 -days 1000 \
+   -sha512 -days 1000 \
    -subj "/C=FR/L=Toulouse/O=Test" \
-   -addext "basicConstraints=CA:true" \
+   -addext "basicConstraints=critical,CA:true,pathlen:0" \
+   -addext "keyUsage=critical,keyCertSign,cRLSign" \
    -key root-CA.key \
    -out root-CA.crt
 
 # Create the certificate for the server on localhost. Note that rustls is finicky, requiring the
 # subjectAltName field to be present.
-openssl req -new -sha256 \
+openssl req -new -sha512 \
    -subj "/C=FR/L=Toulouse/O=Test/CN=localhost" \
    -addext 'subjectAltName=DNS:localhost' \
+   -addext 'basicConstraints=critical,CA:FALSE' \
+   -addext 'extendedKeyUsage=serverAuth' \
    -key localhost-cert.key \
    -out localhost-cert.csr
 openssl x509 -req \
-   -CAcreateserial -days 1000 -sha256 -copy_extensions copy \
+   -CAcreateserial -days 1000 -sha512 -copy_extensions copy \
    -in localhost-cert.csr \
    -CA root-CA.crt \
    -CAkey root-CA.key \
    -out localhost-cert.crt
 
 # create the certificate for the client
-openssl req -new -sha256 -nodes \
+openssl req -new -sha512 -nodes \
    -subj "/C=FR/L=Toulouse/O=Test/CN=local-test-client" \
-   -addext "basicConstraints=CA:false" \
+   -addext "basicConstraints=critical,CA:false" \
    -addext "extendedKeyUsage=clientAuth" \
    -key client-cert.key \
    -out client-cert.csr
 openssl x509 -req \
-   -CAcreateserial -days 1000 -sha256 -copy_extensions copy \
+   -CAcreateserial -days 1000 -sha512 -copy_extensions copy \
    -CA root-CA.crt \
    -CAkey root-CA.key \
    -in client-cert.csr \
